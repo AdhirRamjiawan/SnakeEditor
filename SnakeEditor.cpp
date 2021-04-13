fcc4874d07f5d36ba62c3c748e06b8865fac1488
Adhir Ramjiawan <adhirramjiawan0@gmail.com>
2021-04-13 19:36:29 +0200

    small refactor

--- SnakeEditor.cpp
+++ SnakeEditor.cpp
@@ -16,8 +16,24 @@ float snakeBlockSize = 15.f;
 const int gameWidth = 520;
 const int gameHeight = 400;
 
+int snakeHorizontalDirection = 1;
+int snakeVerticalDirection = 1;
+
+float snakeHorizontalSpeed = 0;
+float snakeVerticalSpeed = 0;
+
+const float snakeMoveWait = 10.f;
+float snakeMoveWaitCount = 0;
+
+bool snakeCollided = false;
+
 bool displayGridEnabled = true;
 sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Snake Game!");
+
+sf::RectangleShape snakeHead(sf::Vector2f(snakeBlockSize, snakeBlockSize));
+sf::RectangleShape target(sf::Vector2f(snakeBlockSize, snakeBlockSize));
+
+vector<sf::RectangleShape> snakeBlocks;
 
 void displayGrid()
 {
@@ -40,26 +56,119 @@ void displayGrid()
     }
 }
 
+void handleInput()
+{
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        // "close requested" event: we close the window
+        if (event.type == sf::Event::Closed)
+            window.close();
+    }
+
+    // THERE'S A BUG THAT ALLOWS YOU TO COLLIDE THE
+    // SNAKE WITH ITSELF. YOU JUST NEED TO PRESS
+    // A VERTICAL BUTTON IN QUICK SUCCESSION TO 
+    // A HORIZONTAL BUTTON. TO FIX INTRODUCE A 
+    // DRAW CALL BOOL OR SOMETHING TO SYNC INPUT
+    // WITH DRAW CALLS.
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        if (snakeHorizontalSpeed == 0)
+        {
+            snakeHorizontalDirection = -1;
+            snakeHorizontalSpeed = snakeBlockSize;
+            snakeVerticalSpeed = 0;
+        }
+
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        if (snakeHorizontalSpeed == 0)
+        {
+            snakeHorizontalDirection = 1;
+            snakeHorizontalSpeed = snakeBlockSize;
+            snakeVerticalSpeed = 0;
+        }
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        if (snakeVerticalSpeed == 0)
+        {
+            snakeVerticalDirection = -1;
+            snakeHorizontalSpeed = 0;
+            snakeVerticalSpeed = snakeBlockSize;
+        }
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        if (snakeVerticalSpeed == 0)
+        {
+            snakeVerticalDirection = 1;
+            snakeHorizontalSpeed = 0;
+            snakeVerticalSpeed = snakeBlockSize;
+        }
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
+    {
+        displayGridEnabled = !displayGridEnabled;
+        cout << "Toggled grid display" << endl;
+    }
+}
+
+void drawGame()
+{
+    window.clear(sf::Color::Black);
+
+    snakeHead.setPosition(sf::Vector2f(
+        snakeHead.getPosition().x + snakeHorizontalSpeed * snakeHorizontalDirection,
+        snakeHead.getPosition().y + snakeVerticalSpeed * snakeVerticalDirection
+    ));
+
+    sf::RectangleShape lastBlock(sf::Vector2f(snakeBlockSize, snakeBlockSize));
+    lastBlock.setPosition(sf::Vector2f(
+        snakeHead.getPosition().x,
+        snakeHead.getPosition().y
+    ));
+
+    snakeBlocks.push_back(lastBlock);
+
+    if (snakeBlocks.size() > snakeLength)
+    {
+        snakeBlocks.erase(snakeBlocks.begin(), snakeBlocks.begin() + 1);
+    }
+
+    if (displayGridEnabled)
+    {
+        displayGrid();
+    }
+
+    for (auto block : snakeBlocks)
+    {
+        window.draw(block);
+    }
+
+    window.draw(snakeHead);
+
+    window.draw(target);
+
+    // end the current frame
+    window.display();
+
+    snakeMoveWaitCount = 0;
+
+    if (snakeCollided)
+    {
+        cout << "snake collided!" << endl;
+    }
+}
+
 int main()
 {
-    
-    sf::Clock clock;
-    sf::RectangleShape snakeHead(sf::Vector2f(snakeBlockSize, snakeBlockSize));
     snakeHead.setFillColor(sf::Color::Green);
-    vector<sf::RectangleShape> snakeBlocks;
-
-    int snakeHorizontalDirection = 1;
-    int snakeVerticalDirection = 1;
+    sf::Clock clock;
     
-    float snakeHorizontalSpeed = 0;
-    float snakeVerticalSpeed = 0;
-
-    const float snakeMoveWait = 10.f;
-    float snakeMoveWaitCount = 0;
-
-    bool snakeCollided = false;
-
-    sf::RectangleShape target(sf::Vector2f(snakeBlockSize, snakeBlockSize));
 
     target.setPosition(sf::Vector2f(10 * snakeBlockSize, 10 * snakeBlockSize));
     target.setFillColor(sf::Color::Red);
@@ -69,105 +178,13 @@ int main()
     {
         sf::Time deltaTime = clock.restart();
 
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            if (snakeHorizontalSpeed == 0)
-            {
-                snakeHorizontalDirection = -1;
-                snakeHorizontalSpeed = snakeBlockSize;
-                snakeVerticalSpeed = 0;
-            }
-            
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            if (snakeHorizontalSpeed == 0)
-            {
-                snakeHorizontalDirection = 1;
-                snakeHorizontalSpeed = snakeBlockSize;
-                snakeVerticalSpeed = 0;
-            }
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            if (snakeVerticalSpeed == 0)
-            {
-                snakeVerticalDirection = -1;
-                snakeHorizontalSpeed = 0;
-                snakeVerticalSpeed = snakeBlockSize;
-            }
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            if (snakeVerticalSpeed == 0)
-            {
-                snakeVerticalDirection = 1;
-                snakeHorizontalSpeed = 0;
-                snakeVerticalSpeed = snakeBlockSize;
-            }
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
-        {
-            displayGridEnabled = !displayGridEnabled;
-            cout << "Toggled grid display" << endl;
-        }
+        handleInput();
 
         float updateDelta = deltaTime.asSeconds();
 
         if (snakeMoveWaitCount > snakeMoveWait)
         {
-            window.clear(sf::Color::Black);
-
-            snakeHead.setPosition(sf::Vector2f(
-                snakeHead.getPosition().x + snakeHorizontalSpeed * snakeHorizontalDirection,
-                snakeHead.getPosition().y + snakeVerticalSpeed * snakeVerticalDirection
-            ));
-
-            sf::RectangleShape lastBlock(sf::Vector2f(snakeBlockSize, snakeBlockSize));
-            lastBlock.setPosition(sf::Vector2f(
-                snakeHead.getPosition().x,
-                snakeHead.getPosition().y
-            ));
-
-            snakeBlocks.push_back(lastBlock);
-
-            if (snakeBlocks.size() > snakeLength)
-            {
-                snakeBlocks.erase(snakeBlocks.begin(), snakeBlocks.begin() + 1);
-            }
-
-            if (displayGridEnabled)
-            {
-                displayGrid();
-            }
-
-            for (auto block : snakeBlocks)
-            {
-                window.draw(block);
-            }
-
-            window.draw(snakeHead);
-
-            window.draw(target);
-
-            // end the current frame
-            window.display();
-
-            snakeMoveWaitCount = 0;
-
-            if (snakeCollided)
-            {
-                cout << "snake collided!" << endl;
-            }
+            drawGame();
         }
         
         snakeMoveWaitCount += 0.0005f;
